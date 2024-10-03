629c0a8f7f8118fb137535da97bbf9a8758b0a78
Thomson1511 <kovacstomi130@gmail.com>
2024-10-03 12:49:28 +0200

    sok minden

--- javascript.js
+++ javascript.js
@@ -1,14 +1,9 @@
 let IcaoCallsigns = [
-    {
-        icaoc: "LHBP",
-        calls: "Budapest",
-        contry: "Hungary"
-    },
-    {
-        icaoc: "LHKE",
-        calls: "Kecskemét",
-        contry: "Hungary"
-    }
+    {icaoc: "AAL",	calls: "American",	contry: "USA"},
+    {icaoc: "AAR",	calls: "Asiana",	contry: "Republic of Korea"},
+    {icaoc: "ABD",	calls: "Atlanta",	contry: "Iceland"},
+    {icaoc: "ABP",	calls: "B-air",	contry: "Czechia"},
+    {icaoc: "ABR",	calls: "Contract",	contry: "Ireland"}
 ];
 
 document.addEventListener('keydown', function (event) {
@@ -16,49 +11,76 @@ document.addEventListener('keydown', function (event) {
         showNext();
     }
     if (event.key === '1'){
-        skip();
+        hint();
     }
 });
 
 const dropdownInput = document.querySelector('.dropdown-input');
-    const dropdownList = document.getElementById('dropdownList');
+const dropdownList = document.getElementById('dropdownList');
+const arrowdown = document.getElementById('dropdownarrow');
+const IcaoHint = document.getElementById("IcaoHint");
+const CallsignHint = document.getElementById("CallsignHint");
+const CoutryHint = document.getElementById("CoutryHint");
+let countryName = '';
 
-    // Megjeleníti a lenyíló menüt, ha az inputra kattintunk
-    dropdownInput.addEventListener('focus', () => {
+//shuffle
+
+let ShuffledCallsigns = [];
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+IcaoCallsigns = shuffle(IcaoCallsigns);
+
+// Megjeleníti a lenyíló menüt, ha az inputra kattintunk
+dropdownInput.addEventListener('focus', () => {
+    dropdownList.style.display = 'block';
+});
+
+arrowdown.addEventListener('click', () => {
+    if (dropdownList.style.display === 'block') {
+        dropdownList.style.display = 'none';
+    } else {
         dropdownList.style.display = 'block';
-    });
+    }
+});
 
-    // Elrejti a lenyíló menüt, ha a felhasználó máshová kattint
-    document.addEventListener('click', (e) => {
-        if (!e.target.closest('.dropdown')) {
-            dropdownList.style.display = 'none';
-        }
-    });
-
-    // Opció kiválasztása
-    function selectOption(element) {
-        dropdownInput.value = element.textContent;
+// Elrejti a lenyíló menüt, ha a felhasználó máshová kattint
+document.addEventListener('click', (e) => {
+    if (!e.target.closest('.dropdown')) {
         dropdownList.style.display = 'none';
     }
+});
 
-    // Opciók szűrése az input mező alapján
-    function filterOptions() {
-        const filter = dropdownInput.value.toLowerCase();
-        const options = dropdownList.getElementsByClassName('dropdown-item');
+// Opció kiválasztása
+function selectOption(element) {
+    countryName = element.querySelector('.country-name').textContent;
+    dropdownInput.value = countryName;
+    dropdownList.style.display = 'none';
+}
+
+
+// Opciók szűrése az input mező alapján
+function filterOptions() {
+    const filter = dropdownInput.value.toLowerCase();
+    const options = dropdownList.getElementsByClassName('dropdown-item');
+
+    for (let i = 0; i < options.length; i++) {
+        const option = options[i];
+        const countryName = option.querySelector('.country-name').textContent.toLowerCase();
         
-        for (let i = 0; i < options.length; i++) {
-            const option = options[i];
-            const text = option.textContent || option.innerText;
-            
-            if (text.toLowerCase().indexOf(filter) > -1) {
-                option.style.display = "";
-            } else {
-                option.style.display = "none";
-            }
+        if (countryName.indexOf(filter) > -1) {
+            option.style.display = "";
+        } else {
+            option.style.display = "none";
         }
     }
-
-
+}
 
 document.addEventListener("DOMContentLoaded", function() {
     showCalls();
@@ -66,8 +88,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
 let currentCallsignIndex = 0;
 let mode = "basic";
-
-//fields
 
 var headerContent = document.getElementById("header");
 var IcaoField = document.getElementById("IcaoField");
@@ -103,24 +123,56 @@ function showNext(){
         if(IcaoCallsigns.length > currentCallsignIndex + 1){
             currentCallsignIndex += 1;
             showCalls();
+            clear()
+            if(checkCountry()){
+                CallsignHint.innerHTML = "";
+            }
+            else{
+                previousHint();
+                CoutryHint.style.backgroundColor = "red";
+            }
         }
         else{
-            done()
+            if(checkCountry()){
+                alert("if")
+                previousHint()
+                CoutryHint.style.backgroundColor = "red";
+                done();
+            }
+            else{
+                alert("else")
+                done();
+                clear();
+            }
         }
     }
     else{
         alert("not good");
     }
+
 }
 
-function skip(){
-    if(IcaoCallsigns.length > currentCallsignIndex + 1){
-        currentCallsignIndex += 1;
-        showCalls();
-    }
-    else{
-        done()
-    }
+function clear(){
+    document.getElementById("Callsign").value = "";
+    document.getElementById("ICAOcode").value = "";
+    document.querySelector(".dropdown-input").value = "";
+    CoutryHint.style.backgroundColor = "white";
+    IcaoHint.innerHTML = "";
+    CoutryHint.innerHTML = "";
+    CallsignHint.innerHTML = "";
+}
+
+function previousHint(){
+    IcaoHint.innerHTML = IcaoCallsigns[currentCallsignIndex - 1].icaoc;
+    CallsignHint.innerHTML = IcaoCallsigns[currentCallsignIndex - 1].calls;
+    CoutryHint.innerHTML = IcaoCallsigns[currentCallsignIndex - 1].contry;
+}
+
+function hint(){
+    IcaoHint.innerHTML = IcaoCallsigns[currentCallsignIndex].icaoc;
+    CallsignHint.innerHTML = IcaoCallsigns[currentCallsignIndex].calls;
+    CoutryHint.innerHTML = IcaoCallsigns[currentCallsignIndex].contry;
+    CoutryHint.style.backgroundColor = "white";
 }
 
 function check(){
@@ -145,6 +197,16 @@ function check(){
         }
     }
 }
+
+function checkCountry() {
+    if(countryName.toLowerCase() === IcaoCallsigns[currentCallsignIndex - 1].contry.toLowerCase()){
+        return true;
+    }
+    else{
+        return false;
+    } 
+}
+
 function done(){
     alert("done");
     currentCallsignIndex = 0;
