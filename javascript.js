38a14222e892a77b44608cf2efeaabe709cedeee
Thomson1511 <kovacstomi130@gmail.com>
2024-10-10 09:05:23 +0200

    refresh after done

--- javascript.js
+++ javascript.js
@@ -507,6 +507,7 @@ function done(){
     nextCallsignIndex = 1;
     errors = 0;
     errorstxt.innerHTML = errors;
+    refresh();
     showCalls();
     if(ErrorList.length < 1){
         alert('Congratulations! You have completed all the constraints.');
